360cd9f60cac21e8e850f0ece54afcddd848b1f8
Isabella Duan <isadua28@bergen.org>
2025-11-17 18:43:22 -0500

    homework completed

---
+++ Lesson03-Parameters/Practice/homework.py
@@ -0,0 +1,33 @@
+# Question 5: Code Tracing 
+"""
+18
+15
+"""
+
+# Question 6: Code Writing
+
+def make_notification(user, *messages, urgent=False): 
+    notifs = ", ".join(messages)
+    if urgent==False: 
+        return f"{user} - {notifs}"
+    return f"URGENT: {user} - {notifs}"
+
+print(make_notification("admin", "Server down!", urgent=True))
+print(make_notification("user", "Welcome", "Check inbox"))
+
+# Question 7: Code Tracing
+"""
+SELECT name, email FROM users LIMIT 10
+SELECT * FROM logs WHERE "level='error'" LIMIT 5
+"""
+# Question 8: Code Writing
+
+def log_action(actor, *actions, timestamp=None, **context): 
+    action = ", ".join(actions)
+    info = ""
+    for key in context: 
+        info += f"{key}={context[key]}, "
+    info = info.rstrip(", ")
+    return f"{actor}: {action} | {info}"
+
+print(log_action("bot", "login", "scan", source="API", ip="1.2.3.4"))
