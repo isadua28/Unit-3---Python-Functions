89a599eb5b576be7c63e57d1fa3495ac4723f248
Isabella Duan <isadua28@bergen.org>
2025-12-01 08:37:37 -0500

    test, math utils, demo complete

---
+++ Lesson06-Entry-Point/math_utils.py
@@ -0,0 +1,8 @@
+def square(n):
+    return n * n
+
+print("Testing1", square(2))
+
+if __name__ == "__main__":
+    print("Testing2", square(5))
+    
---
+++ Lesson06-Entry-Point/test.py
@@ -0,0 +1,3 @@
+# import demo
+from math_utils import square 
+print(f"Damage: {square(10)}")
